91b6fa6cdf6136703adf3bb3d9d6ee69a90f3e74
Amal221200 <amalmurikkoli2000@gmail.com>
2022-02-21 06:37:54 +0530

    styles

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,12 @@
 
 const about = () => {
-  return (
-    <div>about</div>
-  )
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, maxime minima nisi, voluptates dignissimos eaque cum iusto rem perspiciatis numquam iste, non neque laboriosam nemo animi mollitia atque eos libero.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, maxime minima nisi, voluptates dignissimos eaque cum iusto rem perspiciatis numquam iste, non neque laboriosam nemo animi mollitia atque eos libero.</p>
+        </div>
+    )
 }
 
 export default about
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <Link href='/ninjas'><a>See Ninjas</a></Link>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, maxime minima nisi, voluptates dignissimos eaque cum iusto rem perspiciatis numquam iste, non neque laboriosam nemo animi mollitia atque eos libero.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, maxime minima nisi, voluptates dignissimos eaque cum iusto rem perspiciatis numquam iste, non neque laboriosam nemo animi mollitia atque eos libero.</p>
+      <Link href='/ninjas'><a className={styles.btn}>See Ninjas</a></Link>
     </div>
   )
 }
